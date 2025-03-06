dd3160295efb680067f1839a3da24b146125b033
Force1ess <forcelesszheng@foxmail.com>
2025-03-07 00:39:52 +0800

    load tokenizers

---
+++ smolvlm/gsoc.md
@@ -0,0 +1,5 @@
+SmolVLM: https://github.com/huggingface/transformers/blob/bc30dd1efb99f571d45b2e2131a555d09285ddd8/src/transformers/models/smolvlm/modular_smolvlm.py
+Idefics3: https://github.com/huggingface/transformers/blob/main/src/transformers/models/idefics3/modeling_idefics3.py#L616
+Candle Tutorial: https://github.com/ToluClassics/candle-tutorial?tab=readme-ov-file#3-parallels-between-pytorch-and-candle
+Candle NN: https://github.com/huggingface/candle/tree/main/candle-nn/src
+Candle Examples: https://github.com/huggingface/candle/tree/main/candle-examples/examples
--- smolvlm/src/main.rs
+++ smolvlm/src/main.rs
@@ -1,29 +1,22 @@
-use candle_core::{Device, Result, Tensor};
+use tokenizers::Tokenizer;
+use anyhow::Error as E;
 
-struct Model {
-    first: Tensor,
-    second: Tensor,
+pub fn get_tokenizer(tokenizer: Option<String>) -> anyhow::Result<Tokenizer> {
+    let tokenizer = match tokenizer {
+        None => {
+            let api = hf_hub::api::sync::Api::new()?;
+            let api = api.model("HuggingFaceTB/SmolVLM2-256M-Video-Instruct".to_string());
+            api.get("tokenizer.json")?
+        }
+        Some(file) => file.into(),
+    };
+
+    Tokenizer::from_file(tokenizer).map_err(E::msg)
 }
 
-impl Model {
-    fn forward(&self, image: &Tensor) -> Result<Tensor> {
-        let x = image.matmul(&self.first)?;
-        let x = x.relu()?;
-        x.matmul(&self.second)
-    }
-}
-
-fn main() -> Result<()> {
-    // Use Device::new_cuda(0)?; to use the GPU.
-    let device = Device::Cpu;
-
-    let first = Tensor::randn(0f32, 1.0, (784, 100), &device)?;
-    let second = Tensor::randn(0f32, 1.0, (100, 10), &device)?;
-    let model = Model { first, second };
-
-    let dummy_image = Tensor::randn(0f32, 1.0, (1, 784), &device)?;
-
-    let digit = model.forward(&dummy_image)?;
-    println!("Digit {digit:?} digit");
+fn main() -> anyhow::Result<()> {
+    let tokenizer = get_tokenizer(None)?;
+    let tokens = tokenizer.encode("Hello, world!", true).unwrap();
+    println!("{:?}", tokens);
     Ok(())
 }
